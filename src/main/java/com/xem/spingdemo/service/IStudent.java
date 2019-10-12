bb9124689774eca9494ad148904c3e3857509d16
xuermin <xemiracle@163.com>
2019-10-13 01:42:31 +0800

    feat(IStudent): 新增IStudent

---
+++ src/main/java/com/xem/spingdemo/service/IStudent.java
@@ -0,0 +1,18 @@
+package com.xem.spingdemo.service;
+
+/**
+ * @author: xuermin
+ * @description:
+ * @date: 2019/10/13 1:30
+ * @modified by:
+ */
+public interface IStudent {
+    /**
+     * @author: xuermin
+     * @description: 
+     * @param: [a]
+     * @return: java.lang.String
+     * @date: 2019/10/13 1:36
+     */
+    String getName(int a);
+}
